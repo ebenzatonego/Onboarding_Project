3ad7887b8b9787bacaef2d9b6d6de2fab92be38c
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-04-03 10:26:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,21 @@ use Illuminate\Support\Facades\Route;
 // Super-admin
 // Admin
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/video_instruction', function () {
-    return view('video_instruction');
-});
-Auth::routes();
+// Route::get('/', function () {
+//     return view('home');
+// });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
 
 // LOGIN
 Route::middleware(['auth',])->group(function () {
+
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/', 'HomeController@index');
+
+    Route::get('/video_instruction', function () {
+        return view('video_instruction');
+    });
 
 });
 

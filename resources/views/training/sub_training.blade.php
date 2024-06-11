0184b4215fdcc98482b8be3a36f50e330b78531c
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-06-12 00:09:28 +0700

    Update sub_training.blade.php

--- resources/views/training/sub_training.blade.php
+++ resources/views/training/sub_training.blade.php
@@ -5,14 +5,17 @@
         height: 311px;
         min-width: 311px;
         color: #fff;
-
+        padding:4px;
     }
 
     .carousel-fav-course .item img {
         width: 100%;
         height: 100%;
         object-fit: contain;
-
+        border-radius: 10px;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        -webkit-box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        -moz-box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
     }
 
     .owl-theme .owl-dots .owl-dot.active span,

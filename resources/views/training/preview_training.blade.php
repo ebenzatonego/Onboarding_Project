4d2ec317a51938e14ba78d9e0d23d12838dd7339
lucky <54930672+luxlnw171041@users.noreply.github.com>
2024-06-11 22:37:26 +0700

    Update preview_training.blade.php

--- resources/views/training/preview_training.blade.php
+++ resources/views/training/preview_training.blade.php
@@ -1,1 +1,101 @@
-{{ $data_training->title }}
+@extends('layouts.theme_admin')
+
+@section('content')
+
+<style>
+    .phone-frame {
+        min-width: 375px;
+        min-height: 783px;
+        border-radius: 40px;
+        border: 8px #000 solid;
+    }
+
+    .frame-top {
+        display: flex;
+        background-color: #cbcbcb;
+        border-radius: 32px 32px 0 0;
+    }
+
+    .phone-camera {
+        background-color: #000;
+        border-radius: 0 0 15px 15px;
+        width: 100%;
+        height: 30px;
+        position: relative;
+        display: block;
+    }
+
+    .time {
+        font-size: 16px;
+    }
+
+    .phone-website {
+        width: 100%;
+        padding: 10px;
+        background-color: #cbcbcb;
+
+    }
+
+    .azayagencyjourney {
+        display: flex;
+        justify-content: center;
+        padding: 8px;
+        background-color: #9799a0;
+        border-radius: 8px;
+        position: relative;
+    }
+
+    .icon-reload {
+        position: absolute;
+        top: 55%;
+        right: 5px;
+        transform: translate(-50%, -50%);
+    }
+    .content-training{
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+    }
+    .frame-bottom{
+        background-color: #cbcbcb;
+        width: 100%;
+        display: flex;
+        justify-content: space-evenly;
+    }
+</style>
+<div class="container-fluid">
+
+    <div class="d-flex">
+        <div class="phone-frame">
+            <div class="frame-top">
+                <div class="text-dark px-4 time d-flex align-items-center">{{ date('H:i') }}</div>
+                <div class="w-100 phone-camera d-flex align-items-center"> &nbsp;</div>
+                <div class="px-2 d-flex align-items-center">
+                    <i class="text-dark mx-1 fa-solid fa-signal-bars"></i>
+                    <i class="text-dark mx-1 fa-solid fa-wifi"></i>
+                    <i class="text-dark mx-1 fa-solid fa-battery-three-quarters"></i>
+                </div>
+            </div>
+            <div class="phone-website">
+                <div class="azayagencyjourney">
+                    <i class="fa-solid fa-lock me-2 text-dark"></i>
+                    <span class="text-dark">https://azayagencyjourney.com</span>
+
+                    <div class="icon-reload">
+                        <i class="text-dark fa-solid fa-rotate-right"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="content-training">
+
+            </div>
+
+            <div class="frame-bottom">
+
+            </div>
+        </div>
+        <div class="w-100 ps-3">asd</div>
+    </div>
+</div>
+
+@endsection

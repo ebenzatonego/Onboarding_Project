d3c867ba32be70a2158af93bf9d666c893cde9d8
lucky <54930672+luxlnw171041@users.noreply.github.com>
2024-06-12 14:25:26 +0700

    font

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -563,8 +563,8 @@
                     <path d="M128.444 101.75L121.673 89.1863H115.033H108.328H52.0818C49.9987 89.1863 48.1108 88.0796 47.0692 86.3219C46.0276 84.5642 45.9625 82.3508 46.939 80.528L57.8757 60.3472H37.4996H30.7943H24.1542L1.82506 101.75C-0.323223 105.722 -0.193024 110.344 2.08546 114.184C4.55924 118.286 8.986 120.759 13.9336 120.759H116.335C121.283 120.759 125.709 118.286 128.183 114.184C130.462 110.344 130.527 105.722 128.444 101.75Z" fill="#243286" />
                     <path d="M77.2092 7.03074C74.8656 2.66908 70.2435 0 65.1007 0C59.9578 0 55.3358 2.66908 52.9922 7.03074L30.4678 48.6944H37.1079H43.7481H67.6396C69.7227 48.6944 71.6106 49.8011 72.6522 51.5588C73.6938 53.3165 73.7589 55.5298 72.7824 57.3526L61.8457 77.5335H101.882H108.522H115.162L77.2092 7.03074Z" fill="#243286" />
                 </svg>
-                <p style="color:#0E2B81;font-size: 32px;font-weight: bolder;margin: 10px 0 0 0;">Agency Journey</p>
-                <p style="color:#0E2B81;font-size: 10px;font-weight: bold;margin: 0;">ALLIANZ ON-BOARDING WEB</p>
+                <p class="AllianzNeo" style="color:#0E2B81;font-size: 32px;font-weight: bolder;margin: 10px 0 0 0;">AGENCY JOURNEY</p>
+                <p class="AllianzNeo" style="color:#0E2B81;font-size: 10px;font-weight: bold;margin: 0;">ALLIANZ ON-BOARDING WEB</p>
             </div>
         </div>
         <div class="authentication-bottom col-lg-6">

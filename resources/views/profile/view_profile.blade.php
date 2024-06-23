c6f1b7046afd5d13f221b03b926274af77f69d60
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-06-24 00:42:01 +0700

    Update view_profile.blade.php

--- resources/views/profile/view_profile.blade.php
+++ resources/views/profile/view_profile.blade.php
@@ -444,6 +444,15 @@
                     </div>
                 </div>
             </div>
+
+            @if(in_array(Auth::user()->role, ['Super-admin', 'Admin']))
+            <center>
+                <a href="{{ url('/welcome_admin') }}" class="btn btn-sm btn-info mb-3" style="width:80%;">
+                    For <b>{{ Auth::user()->role }}</b>
+                </a>
+            </center>
+            @endif
+            
             <div class="col-lg-8 p-0 p-lg-3">
                 <div class="card-profile p-3">
                     <div class="card-body ">
@@ -655,14 +664,6 @@
 
 
                 </div>
-
-                @if(in_array(Auth::user()->role, ['Super-admin', 'Admin']))
-                <center>
-                    <a href="{{ url('/welcome_admin') }}" class="btn btn-sm btn-info mb-3" style="width:80%;">
-                        For <b>{{ Auth::user()->role }}</b>
-                    </a>
-                </center>
-                @endif
 
                 <div class="contact-leader mb-5">
                     @if( !empty($users->account_upper_al) )

4939ebac19532d2ffb334d4af543b1fd63604d45
lucky <54930672+luxlnw171041@users.noreply.github.com>
2024-07-03 00:02:26 +0700

    Update index.blade.php

--- resources/views/calendars/index.blade.php
+++ resources/views/calendars/index.blade.php
@@ -321,7 +321,7 @@
         }
 
         .appointment {
-            padding:  0 20px 20px 60px!important;
+            padding:  0 20px 20px 80px!important;
             overflow: auto;
             transition: all .15s ease-in-out;
         }

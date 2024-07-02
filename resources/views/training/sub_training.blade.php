76ac495ecde66c551d8f895c84d93bc3f51ad9e2
lucky <54930672+luxlnw171041@users.noreply.github.com>
2024-07-02 21:17:35 +0700

    Update sub_training.blade.php

--- resources/views/training/sub_training.blade.php
+++ resources/views/training/sub_training.blade.php
@@ -337,22 +337,10 @@
                     }
 
                     $('.carousel-fav-course').owlCarousel({
-                        // stagePadding:20,
                         loop: false,
                         autoWidth: true,
-                        margin: 10,
-                        nav: false,
-                        responsive: {
-                            0: {
-                                items: 1
-                            },
-                            600: {
-                                items: 3
-                            },
-                            1000: {
-                                items: 1
-                            }
-                        }
+                        nav: false,    
+                        dots: false,
                     })
 
                 }

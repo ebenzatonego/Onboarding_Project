bae146948996162bd4a4174d7e19136118f530df
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-07-03 20:57:51 +0700

    Update video_instruction.blade.php

--- resources/views/video_instruction.blade.php
+++ resources/views/video_instruction.blade.php
@@ -409,7 +409,7 @@
                 </div>
 
                 <div class="d-flex-justify-content-center w-100 px-3">
-                    <video id="tag_video_intro" src="" controls autoplay loop playsinline style="width:100%;border-radius: 10px; max-width: 628px;" class="video-preview"></video>
+                    <video id="tag_video_intro" src="" controls autoplay loop muted playsinline style="width:100%;border-radius: 10px; max-width: 628px;" class="video-preview"></video>
                 </div>
             </div>
         </div>

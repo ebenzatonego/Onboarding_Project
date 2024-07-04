e67b2f41d07a499a5c818e0fb4dcf4a9dd25bc0b
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-07-04 22:41:46 +0700

    Check_open_training

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
         Commands\Cancel_check_birthday::class,
         Commands\Check_license_expire::class,
         Commands\Reset_content_popup::class,
+        Commands\Check_open_training::class,
     ];
 
     /**
@@ -30,6 +31,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('cron:cancel_check_birthday')->dailyAt('00:01')->withoutOverlapping(5);
         $schedule->command('cron:check_license_expire')->dailyAt('01:00')->withoutOverlapping(5);
         $schedule->command('cron:reset_content_popup')->dailyAt('03:00')->withoutOverlapping(5);
+        $schedule->command('cron:check_open_training')->everyMinute()->withoutOverlapping(5);
     }
 
     /**

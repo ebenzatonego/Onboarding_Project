6f0598b91126b9aae2719d6ae34a6fbc3cbfec9e
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-07-05 00:04:27 +0700

    ..

--- app/Console/Commands/Check_open_training.php
+++ app/Console/Commands/Check_open_training.php
@@ -49,16 +49,16 @@ class Check_open_training extends Command
         // อัพเดต status เป็น 'Yes' ถ้าวันเวลาปัจจุบันอยู่ระหว่าง datetime_start และ datetime_end
         DB::table('trainings')
             ->where('datetime_start', '<=', $currentDateTime)
-            ->where('datetime_end', '>=', $currentDateTime)
+            // ->where('datetime_end', '>=', $currentDateTime)
             ->update(['status' => 'Yes']);
 
         // อัพเดต status เป็น null ถ้าวันเวลาปัจจุบันไม่อยู่ระหว่าง datetime_start และ datetime_end
-        DB::table('trainings')
-            ->where(function ($query) use ($currentDateTime) {
-                $query->where('datetime_start', '>', $currentDateTime)
-                      ->orWhere('datetime_end', '<', $currentDateTime);
-            })
-            ->update(['status' => null]);
+        // DB::table('trainings')
+        //     ->where(function ($query) use ($currentDateTime) {
+        //         $query->where('datetime_start', '>', $currentDateTime)
+        //               ->orWhere('datetime_end', '<', $currentDateTime);
+        //     })
+        //     ->update(['status' => null]);
     }
 
 }

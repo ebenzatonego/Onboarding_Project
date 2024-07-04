355816071268313d88c17bbefb78b10db6f4df45
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-07-04 18:43:59 +0700

    Update video_instruction.blade.php

--- resources/views/video_instruction.blade.php
+++ resources/views/video_instruction.blade.php
@@ -490,6 +490,22 @@
             window.location.href = "{{ url('/home') }}";
         });
 
+        var main_video = document.getElementById('tag_video_intro');
+
+        function isIOS() {
+            return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+        }
+
+        if (isIOS()) {
+            main_video.volume = 1.0; // ตั้งค่าเสียงให้เต็ม
+        }
+
+        main_video.addEventListener('play', function() {
+            if (isIOS()) {
+                main_video.muted = false;
+            }
+        });
+
     });
 
 

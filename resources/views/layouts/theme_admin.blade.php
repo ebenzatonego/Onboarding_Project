52aa88fe86ab286fd02ab5b4840fa4e01e895a04
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-08-04 23:13:11 +0700

    Update theme_admin.blade.php

--- resources/views/layouts/theme_admin.blade.php
+++ resources/views/layouts/theme_admin.blade.php
@@ -5,7 +5,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- Favicons -->
     <link href="{{ url('/img/logo/LOGO (2).png') }}" rel="icon">
     <link href="{{ url('/img/logo/LOGO (2).png') }}" rel="apple-touch-icon">
